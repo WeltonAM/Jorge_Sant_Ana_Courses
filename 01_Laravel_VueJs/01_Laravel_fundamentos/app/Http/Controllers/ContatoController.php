4cefc71e61b62494359efc3176ad4d0de9fb8ff6
Welton Almeida de Matos <74322996+WeltonAM@users.noreply.github.com>
2024-03-12 21:33:39 -0300

    Criação das views: sobre, principal e contato

--- 01_Laravel_VueJs/01_Laravel_fundamentos/app/Http/Controllers/ContatoController.php
+++ 01_Laravel_VueJs/01_Laravel_fundamentos/app/Http/Controllers/ContatoController.php
@@ -7,6 +7,6 @@ use Illuminate\Http\Request;
 class ContatoController extends Controller
 {
     public function contato() {
-        return view('welcome');
+        return view('site.contato');
     }
 }
