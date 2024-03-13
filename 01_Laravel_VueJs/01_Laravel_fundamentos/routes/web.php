4c4689173942f6c79e17ec8d4b66b33b7af2421b
Welton Almeida de Matos <74322996+WeltonAM@users.noreply.github.com>
2024-03-12 21:24:41 -0300

    Criação dos controllers: sobre, principal e contato

--- 01_Laravel_VueJs/01_Laravel_fundamentos/routes/web.php
+++ 01_Laravel_VueJs/01_Laravel_fundamentos/routes/web.php
@@ -1,26 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PrincipalController;
+use App\Http\Controllers\SobreNosController;
+use App\Http\Controllers\ContatoController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+Route::get('/', [PrincipalController::class, 'principal'])->name('home');
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/sobre', [SobreNosController::class, 'sobre'])->name('sobre');
 
-Route::get('/sobre', function () {
-    return view('welcome');
-});
-
-Route::get('/contato', function () {
-    return view('welcome');
-});
+Route::get('/contato', [ContatoController::class, 'contato'])->name('contato');
