f65ebb35301f25ef8de2980ec37f0c8c7e5ae008
Welton Almeida de Matos <74322996+WeltonAM@users.noreply.github.com>
2024-03-26 15:33:52 -0300

    Request validations on `MarcaController`

--- 01_Laravel_VueJs/04_locadora_api-rest/app/Http/Controllers/MarcaController.php
+++ 01_Laravel_VueJs/04_locadora_api-rest/app/Http/Controllers/MarcaController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Marca;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class MarcaController extends Controller
 {
@@ -20,8 +21,26 @@ class MarcaController extends Controller
         return response()->json($marcas, 200);
     }
 
+    protected function validateRequest(Request $request)
+    {
+        return Validator::make($request->all(), [
+            'nome' => 'required|unique:marcas',
+            'imagem' => 'required'
+        ], [
+            'required' => 'O campo [:attribute] é obrigatório.',
+            'nome.unique' => 'O [nome] da marca já existe.',
+        ]);
+    }
+
     public function store(Request $request)
     {
+        $validator = $this->validateRequest($request);
+
+        if ($validator->fails()) {
+            $errors = $validator->errors()->all();
+            return response()->json(['erro' => $errors], 422);
+        }
+
         $marca = $this->marca->create($request->all());
         return response()->json($marca, 201);
     }
@@ -39,6 +58,13 @@ class MarcaController extends Controller
 
     public function update(Request $request, Int $id)
     {
+        $validator = $this->validateRequest($request);
+
+        if ($validator->fails()) {
+            $errors = $validator->errors()->all();
+            return response()->json(['erro' => $errors], 422);
+        }
+
         $marca = $this->marca->find($id);
 
         if (!$marca) {
