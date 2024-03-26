bdaa06500919f68331ff9684a3267c1688cf39ab
Welton Almeida de Matos <74322996+WeltonAM@users.noreply.github.com>
2024-03-26 10:55:41 -0300

    Creating `Marca` migration, model and controller

---
+++ 01_Laravel_VueJs/04_locadora_api-rest/app/Http/Controllers/MarcaController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Marca;
+use Illuminate\Http\Request;
+
+class MarcaController extends Controller
+{
+    public function index()
+    {
+        //
+    }
+
+    public function create()
+    {
+        //
+    }
+
+    public function store(Request $request)
+    {
+        //
+    }
+
+    public function show(Marca $marca)
+    {
+        //
+    }
+
+    public function edit(Marca $marca)
+    {
+        //
+    }
+
+    public function update(Request $request, Marca $marca)
+    {
+        //
+    }
+
+    public function destroy(Marca $marca)
+    {
+        //
+    }
+}
