ca3f55ec214f940020abd01327978f494c8f1da8
Welton Almeida de Matos <74322996+WeltonAM@users.noreply.github.com>
2024-03-26 11:35:28 -0300

    Api routes to `Marca`
    Configuring `MarcaController`

--- 01_Laravel_VueJs/04_locadora_api-rest/routes/api.php
+++ 01_Laravel_VueJs/04_locadora_api-rest/routes/api.php
@@ -14,3 +14,5 @@ Route::apiResource('/carro', 'App\Http\Controllers\CarroController');
 Route::apiResource('/locacao', 'App\Http\Controllers\LocacaoController');
 
 Route::apiResource('/marca', 'App\Http\Controllers\MarcaController');
+
+Route::apiResource('/modelo', 'App\Http\Controllers\ModeloController');
