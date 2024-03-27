32ace7d3b08f5581b8655c424bdd2f23368fd4dc
Welton Almeida de Matos <74322996+WeltonAM@users.noreply.github.com>
2024-03-27 11:49:52 -0300

    Implementing `MarcaRepository`
    Adjusting the Design Pattern concepts to the app

--- 01_Laravel_VueJs/04_locadora_api-rest/app/Http/Controllers/MarcaController.php
+++ 01_Laravel_VueJs/04_locadora_api-rest/app/Http/Controllers/MarcaController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Marca;
+use App\Repositories\MarcaRepository;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
 
@@ -17,35 +18,25 @@ class MarcaController extends Controller
 
     public function index(Request $request)
     {
-        $marcas = [];
+        $marcaRepository = new MarcaRepository($this->marca);
 
         if ($request->has('atributos_modelos')) {
-            $atributos_modelos = $request->atributos_modelos;
+            $atributos_modelos = 'modelos:id,' . $request->atributos_modelos;
 
-            $marcas = $this->marca->with('modelos:id,' . $atributos_modelos);
+            $marcaRepository->selectAtributosRelacionados($atributos_modelos);
         } else {
-            $marcas = $this->marca->with('modelos');
+            $marcaRepository->selectAtributosRelacionados('modelos');
         }
 
         if ($request->has('filtro')) {
-            $filtros = explode(';', $request->filtro);
-
-            foreach ($filtros as $condicao) {
-                $c = explode(':', $condicao);
-
-                $marcas = $marcas->where($c[0], $c[1], $c[2]);
-            }
+            $marcaRepository->filtro($request->filtro);
         }
 
         if ($request->has('atributos')) {
-            $atributos = $request->atributos;
-
-            $marcas = $marcas->selectRaw($atributos)->get();
-        } else {
-            $marcas = $marcas->get();
+            $marcaRepository->selectAtributos($request->atributos);
         }
 
-        return response()->json($marcas, 200);
+        return response()->json($marcaRepository->getResultado(), 200);
     }
 
     public function store(Request $request)
