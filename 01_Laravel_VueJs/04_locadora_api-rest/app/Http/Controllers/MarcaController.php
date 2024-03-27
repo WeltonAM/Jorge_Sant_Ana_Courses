ffe6c7a29a6c3d8a2ece13d4099266335c22d380
Welton Almeida de Matos <74322996+WeltonAM@users.noreply.github.com>
2024-03-27 11:05:05 -0300

    Adding filters to the `Marca` search

--- 01_Laravel_VueJs/04_locadora_api-rest/app/Http/Controllers/MarcaController.php
+++ 01_Laravel_VueJs/04_locadora_api-rest/app/Http/Controllers/MarcaController.php
@@ -15,9 +15,36 @@ class MarcaController extends Controller
         $this->marca = $marca;
     }
 
-    public function index()
+    public function index(Request $request)
     {
-        $marcas = $this->marca->with('modelos')->get();
+        $marcas = [];
+
+        if ($request->has('atributos_modelos')) {
+            $atributos_modelos = $request->atributos_modelos;
+
+            $marcas = $this->marca->with('modelos:id,' . $atributos_modelos);
+        } else {
+            $marcas = $this->marca->with('modelos');
+        }
+
+        if ($request->has('filtro')) {
+            $filtros = explode(';', $request->filtro);
+
+            foreach ($filtros as $condicao) {
+                $c = explode(':', $condicao);
+
+                $marcas = $marcas->where($c[0], $c[1], $c[2]);
+            }
+        }
+
+        if ($request->has('atributos')) {
+            $atributos = $request->atributos;
+
+            $marcas = $marcas->selectRaw($atributos)->get();
+        } else {
+            $marcas = $marcas->get();
+        }
+
         return response()->json($marcas, 200);
     }
 
