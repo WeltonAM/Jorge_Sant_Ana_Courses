6f9e7b5a34ebe1e233bbddae84d94ff3f487ab5d
Welton Almeida de Matos <74322996+WeltonAM@users.noreply.github.com>
2024-03-27 22:10:37 -0300

    Implementing `Cliente`'s services

--- 01_Laravel_VueJs/04_locadora_api-rest/app/Http/Controllers/ClienteController.php
+++ 01_Laravel_VueJs/04_locadora_api-rest/app/Http/Controllers/ClienteController.php
@@ -3,33 +3,101 @@
 namespace App\Http\Controllers;
 
 use App\Models\Cliente;
-use App\Http\Requests\StoreClienteRequest;
-use App\Http\Requests\UpdateClienteRequest;
+use App\Repositories\ClienteRepository;
+use Illuminate\Http\Request;
 
 class ClienteController extends Controller
 {
-    public function index()
+    protected $cliente;
+
+    public function __construct(Cliente $cliente)
     {
-        //
+        $this->cliente = $cliente;
     }
 
-    public function store(StoreClienteRequest $request)
+    public function index(Request $request)
     {
-        //
+        $clienteRepository = new ClienteRepository($this->cliente);
+
+        if ($request->has('atributos_locacoes')) {
+            $atributos_locacoes = 'locacoes:id,' . $request->atributos_locacoes;
+
+            $clienteRepository->selectAtributosRelacionados($atributos_locacoes);
+        } else {
+            $clienteRepository->selectAtributosRelacionados('locacoes');
+        }
+
+        if ($request->has('filtro')) {
+            $clienteRepository->filtro($request->filtro);
+        }
+
+        if ($request->has('atributos')) {
+            $clienteRepository->selectAtributos($request->atributos);
+        }
+
+        return response()->json($clienteRepository->getResultado(), 200);
     }
 
-    public function show(Cliente $cliente)
+    public function store(Request $request)
     {
-        //
+        $request->validate($this->cliente->rules(), $this->cliente->feedback());
+
+        $cliente = $this->cliente->create([
+            'nome' => $request->nome,
+        ]);
+
+        return response()->json($cliente, 201);
     }
 
-    public function update(UpdateClienteRequest $request, Cliente $cliente)
+    public function show(Int $id)
     {
-        //
+        $cliente = $this->cliente->with('locacoes')->find($id);
+
+        if (!$cliente) {
+            return response()->json(['erro' => 'Cliente não encontrado.'], 404);
+        }
+
+        return response()->json($cliente, 200);
     }
 
-    public function destroy(Cliente $cliente)
+    public function update(Request $request, Int $id)
     {
-        //
+        $cliente = $this->cliente->find($id);
+
+        if (!$cliente) {
+            return response()->json(['erro' => 'Cliente não encontrado.'], 404);
+        }
+
+        if ($request->method() === 'PATCH') {
+
+            $regrasDinamicas = array();
+
+            foreach ($cliente->rules() as $input => $regra) {
+                if (array_key_exists($input, $request->all())) {
+                    $regrasDinamicas[$input] = $regra;
+                }
+            }
+
+            $request->validate($regrasDinamicas, $cliente->feedback());
+        } else {
+            $request->validate($cliente->rules(), $cliente->feedback());
+        }
+
+        $cliente->fill($request->all());
+        $cliente->save();
+
+        return response()->json($cliente, 200);
+    }
+
+    public function destroy(Int $id)
+    {
+        $cliente = $this->cliente->find($id);
+
+        if (!$cliente) {
+            return response()->json(['erro' => 'Cliente não encontrado.'], 404);
+        }
+
+        $cliente->delete();
+        return response()->json(['msg' => 'O Cliente foi removido com sucesso!'], 200);
     }
 }
--- 01_Laravel_VueJs/04_locadora_api-rest/app/Models/Cliente.php
+++ 01_Laravel_VueJs/04_locadora_api-rest/app/Models/Cliente.php
@@ -8,4 +8,26 @@ use Illuminate\Database\Eloquent\Model;
 class Cliente extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['nome'];
+
+    public function rules()
+    {
+        return [
+            'nome' => 'required|min:3',
+        ];
+    }
+
+    public function feedback()
+    {
+        return [
+            'required' => 'O campo [:attribute] é obrigatório.',
+            'nome.min' => 'O [nome] deve ter no mínimo 3 caracteres.',
+        ];
+    }
+
+    // public function locacoes()
+    // {
+    //     return $this->belongsTo('App\Models\Locacao');
+    // }
 }
