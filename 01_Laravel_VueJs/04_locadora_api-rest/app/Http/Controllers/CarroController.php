b20d47ceec3257007b0648453363dac1acc2983f
Welton Almeida de Matos <74322996+WeltonAM@users.noreply.github.com>
2024-03-27 21:51:38 -0300

    Procedure `Carro`'s services

--- 01_Laravel_VueJs/04_locadora_api-rest/app/Http/Controllers/CarroController.php
+++ 01_Laravel_VueJs/04_locadora_api-rest/app/Http/Controllers/CarroController.php
@@ -3,33 +3,104 @@
 namespace App\Http\Controllers;
 
 use App\Models\Carro;
-use App\Http\Requests\StoreCarroRequest;
-use App\Http\Requests\UpdateCarroRequest;
+use App\Repositories\CarroRepository;
+use Illuminate\Http\Request;
 
 class CarroController extends Controller
 {
-    public function index()
+    protected $carro;
+
+    public function __construct(Carro $carro)
     {
-        //
+        $this->carro = $carro;
     }
 
-    public function store(StoreCarroRequest $request)
+    public function index(Request $request)
     {
-        //
+        $carroRepository = new CarroRepository($this->carro);
+
+        if ($request->has('atributos_modelo')) {
+            $atributos_modelo = 'modelo:id,' . $request->atributos_modelo;
+
+            $carroRepository->selectAtributosRelacionados($atributos_modelo);
+        } else {
+            $carroRepository->selectAtributosRelacionados('modelo');
+        }
+
+        if ($request->has('filtro')) {
+            $carroRepository->filtro($request->filtro);
+        }
+
+        if ($request->has('atributos')) {
+            $carroRepository->selectAtributos($request->atributos);
+        }
+
+        return response()->json($carroRepository->getResultado(), 200);
     }
 
-    public function show(Carro $carro)
+    public function store(Request $request)
     {
-        //
+        $request->validate($this->carro->rules(), $this->carro->feedback());
+
+        $carro = $this->carro->create([
+            'modelo_id' => $request->modelo_id,
+            'placa' => $request->placa,
+            'disponivel' => $request->disponivel,
+            'km' => $request->km,
+        ]);
+
+        return response()->json($carro, 201);
     }
 
-    public function update(UpdateCarroRequest $request, Carro $carro)
+    public function show(Int $id)
     {
-        //
+        $carro = $this->carro->with('modelo')->find($id);
+
+        if (!$carro) {
+            return response()->json(['erro' => 'Carro não encontrado.'], 404);
+        }
+
+        return response()->json($carro, 200);
     }
 
-    public function destroy(Carro $carro)
+    public function update(Request $request, Int $id)
     {
-        //
+        $carro = $this->carro->find($id);
+
+        if (!$carro) {
+            return response()->json(['erro' => 'Carro não encontrado.'], 404);
+        }
+
+        if ($request->method() === 'PATCH') {
+
+            $regrasDinamicas = array();
+
+            foreach ($carro->rules() as $input => $regra) {
+                if (array_key_exists($input, $request->all())) {
+                    $regrasDinamicas[$input] = $regra;
+                }
+            }
+
+            $request->validate($regrasDinamicas, $carro->feedback());
+        } else {
+            $request->validate($carro->rules(), $carro->feedback());
+        }
+
+        $carro->fill($request->all());
+        $carro->save();
+
+        return response()->json($carro, 200);
+    }
+
+    public function destroy(Int $id)
+    {
+        $carro = $this->carro->find($id);
+
+        if (!$carro) {
+            return response()->json(['erro' => 'Carro não encontrado.'], 404);
+        }
+
+        $carro->delete();
+        return response()->json(['msg' => 'O carro foi removido com sucesso!'], 200);
     }
 }
--- 01_Laravel_VueJs/04_locadora_api-rest/app/Models/Carro.php
+++ 01_Laravel_VueJs/04_locadora_api-rest/app/Models/Carro.php
@@ -8,4 +8,30 @@ use Illuminate\Database\Eloquent\Model;
 class Carro extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['modelo_id', 'placa', 'disponivel', 'km'];
+
+    public function rules()
+    {
+        return [
+            'modelo_id' => 'exists:modelos,id',
+            'placa' => 'required',
+            'disponivel' => 'required',
+            'km' => 'required',
+        ];
+    }
+
+    public function feedback()
+    {
+        return [
+            'required' => 'O campo [:attribute] é obrigatório.',
+
+            'modelo_id.exists' => 'O [Modelo] não foi identificado.',
+        ];
+    }
+
+    public function modelo()
+    {
+        return $this->belongsTo('App\Models\Modelo');
+    }
 }
