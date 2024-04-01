75dcea35c23a37e4d83f10e99e13e91ab49ab7a2
Welton Almeida de Matos <74322996+WeltonAM@users.noreply.github.com>
2024-04-01 20:27:14 -0300

    configuring authorization-protected routes

--- 01_Laravel_VueJs/04_locadora_api-rest/routes/api.php
+++ 01_Laravel_VueJs/04_locadora_api-rest/routes/api.php
@@ -7,15 +7,15 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::apiResource('/cliente', 'App\Http\Controllers\ClienteController');
+Route::apiResource('/cliente', 'App\Http\Controllers\ClienteController')->middleware('jwt.auth');
 
-Route::apiResource('/carro', 'App\Http\Controllers\CarroController');
+Route::apiResource('/carro', 'App\Http\Controllers\CarroController')->middleware('jwt.auth');
 
-Route::apiResource('/locacao', 'App\Http\Controllers\LocacaoController');
+Route::apiResource('/locacao', 'App\Http\Controllers\LocacaoController')->middleware('jwt.auth');
 
-Route::apiResource('/marca', 'App\Http\Controllers\MarcaController');
+Route::apiResource('/marca', 'App\Http\Controllers\MarcaController')->middleware('jwt.auth');
 
-Route::apiResource('/modelo', 'App\Http\Controllers\ModeloController');
+Route::apiResource('/modelo', 'App\Http\Controllers\ModeloController')->middleware('jwt.auth');
 
 Route::post('/login', 'App\Http\Controllers\AuthController@login');
 Route::post('/logout', 'App\Http\Controllers\AuthController@logout');
