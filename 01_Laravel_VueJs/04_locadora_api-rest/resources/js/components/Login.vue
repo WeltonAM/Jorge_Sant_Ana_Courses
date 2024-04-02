2573c32d9eb70214a53f476c98f829f34a72e770
Welton Almeida de Matos <74322996+WeltonAM@users.noreply.github.com>
2024-04-02 19:19:49 -0300

    Implementing web session with JWT

--- 01_Laravel_VueJs/04_locadora_api-rest/resources/js/components/Login.vue
+++ 01_Laravel_VueJs/04_locadora_api-rest/resources/js/components/Login.vue
@@ -6,13 +6,14 @@
                     <div class="card-header">Login</div>
 
                     <div class="card-body">
-                        <form method="POST" action="">
+                        <form method="POST" action="" @submit.prevent="login($event)">
                             <input type="hidden" name="_token" :value="csrf_token">
                             <div class="row mb-3">
                                 <label for="email" class="col-md-4 col-form-label text-md-end">Email</label>
 
                                 <div class="col-md-6">
-                                    <input id="email" type="email" class="form-control" name="email" value="" required autocomplete="email" autofocus>
+                                    <input id="email" type="email" class="form-control" name="email" value="" required
+                                        autocomplete="email" autofocus v-model="email">
                                 </div>
                             </div>
 
@@ -20,7 +21,8 @@
                                 <label for="password" class="col-md-4 col-form-label text-md-end">Senha</label>
 
                                 <div class="col-md-6">
-                                    <input id="password" type="password" class="form-control" name="password" required autocomplete="current-password">
+                                    <input id="password" type="password" class="form-control" name="password" required
+                                        autocomplete="current-password" v-model="password">
                                 </div>
                             </div>
 
@@ -42,9 +44,9 @@
                                         Login
                                     </button>
 
-                                        <a class="btn btn-link" href="">
-                                            Esqueci minha senha
-                                        </a>
+                                    <a class="btn btn-link" href="">
+                                        Esqueci minha senha
+                                    </a>
                                 </div>
                             </div>
                         </form>
@@ -56,10 +58,36 @@
 </template>
 
 <script>
-    export default {
-        props: ['csrf_token'],
-        mounted() {
-            console.log('Component mounted.')
+export default {
+    props: ['csrf_token'],
+    data() {
+        return {
+            email: '',
+            password: '',
         }
-    }
+    },
+    methods: {
+        async login(e) {
+            let url = 'http://localhost:8000/api/login'
+
+            let config = {
+                method: 'POST',
+                body: new URLSearchParams({
+                    'email': this.email,
+                    'password': this.password,
+                })
+            }
+
+            const res = await fetch(url, config)
+            const data = await res.json()
+
+            if (!res.ok) {
+                return data
+            }
+
+            document.cookie = `token=${data[1]};SameSite=Lax`
+            e.target.submit()
+        }
+    },
+}
 </script>
