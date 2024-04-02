bcc5e237a82af945e0ebf962677c3aabb9c6d609
Welton Almeida de Matos <74322996+WeltonAM@users.noreply.github.com>
2024-04-02 19:26:07 -0300

    Creating the `Home` as a vue component

--- 01_Laravel_VueJs/04_locadora_api-rest/resources/js/app.js
+++ 01_Laravel_VueJs/04_locadora_api-rest/resources/js/app.js
@@ -18,6 +18,9 @@ const app = createApp({});
 import Login from './components/Login.vue';
 app.component('login-component', Login);
 
+import Home from './components/Home.vue';
+app.component('home-component', Home);
+
 /**
  * The following block of code may be used to automatically register your
  * Vue components. It will recursively scan this directory for the Vue
